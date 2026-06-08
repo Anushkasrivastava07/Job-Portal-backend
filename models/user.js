06031c48ebedf0ad2eb7f0fb74e2cf8c836009e5
Anushkasrivastava07 <anushkasrivastava020@gmail.com>
2026-06-08 14:19:31 +0530

    Fix Typo monggoose to mongoose

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const userSchema = new monggoose.Schema({
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
